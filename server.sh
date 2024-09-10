415b04f211b59471b7733dad4a3416dff4bac245
nosafearea <nosafearea@outlook.com>
2024-09-10 18:32:50 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,3 @@
-echo '--------------- IP -----------------'$(curl ifconfig.me)
-cat alamatsoket
+#echo '--------------- IP -----------------'$(curl ifconfig.me)
+#cat alamatsoket
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
