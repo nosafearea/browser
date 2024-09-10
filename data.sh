80f2cb52d46f2c9dee0dbf29ddcdf49a8b19025d
nosafearea <nosafearea@outlook.com>
2024-09-10 16:45:20 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,4 +3,6 @@
 #./ngrok authtoken 2Qyuo1u3hVJfyJ1hNbla2KvKZkS_6We9aFPeTqXZMhmH5WSvm > /dev/null 2>&1
 #chmod +x ngrok
 #./ngrok http 33440 >/dev/null 2>&1 >/dev/null &
-#docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
+npm install -g localtunnel && lt --port 7777 > alamatsoket &
+docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
+sleep 30
